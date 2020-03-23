bdd3ad80e79941d343e8b08fd2533dc758a7a89e
vict0rsch <vsch@pm.me>
2020-03-23 12:48:40 -0400

    cleanup

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,10 +6,8 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-console.log('object');
+// chrome.extension.onMessage.addListener(
+//   function (request, sender, sendResponse) {
+//     chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
