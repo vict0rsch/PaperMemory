4d473042aa9f91a3153f416867ec2c060c08bc9f
vict0rsch <vsch@pm.me>
2021-10-25 19:43:57 -0400

    async toplevel call

--- src/popup/theme.js
+++ src/popup/theme.js
@@ -1,17 +1,19 @@
-const __theme = async () => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.get("checkDarkMode", (data) => {
-            resolve(data && data["checkDarkMode"]);
+(async () => {
+    const theme = async () => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get("checkDarkMode", (data) => {
+                resolve(data && data["checkDarkMode"]);
+            });
         });
-    });
-};
+    };
 
-const darkMode = await __theme();
+    const darkMode = await theme();
 
-if (darkMode) {
-    var link = document.createElement("link");
-    link.href = chrome.runtime.getURL("src/popup/popup-css/dark.min.css");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    document.head.appendChild(link);
-}
+    if (darkMode) {
+        var link = document.createElement("link");
+        link.href = chrome.runtime.getURL("src/popup/popup-css/dark.min.css");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        document.head.appendChild(link);
+    }
+})();
