802adf1eb82e17c56d7c7016281a5c60c88bd071
vict0rsch <vsch@pm.me>
2021-11-23 23:37:16 -0500

    add title to delete button

--- src/popup/popup-js/templates.js
+++ src/popup/popup-js/templates.js
@@ -220,7 +220,7 @@ const getPopupEditFormHTML = (paper) => {
                     <label class="label" for="checkFavorite">Favorite: </label>
                     <input ${checked} class="switch" type="checkbox" id="checkFavorite--${id}" name="checkFavorite" value="checkFavorite">
                 </div>
-                <div id="popup-delete-paper">
+                <div id="popup-delete-paper" title="Delete paper from Memory">
                     <svg width="25" height="25" viewBox="0 0 24 24" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke="white">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <line x1="4" y1="7" x2="20" y2="7" />
