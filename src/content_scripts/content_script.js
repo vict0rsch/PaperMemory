4b694b6ab5d3ad39d3d1f3212559953bfe675c11
vict0rsch <vsch@pm.me>
2022-03-16 10:33:10 +0100

    improve logs

--- src/content_scripts/content_script.js
+++ src/content_scripts/content_script.js
@@ -344,22 +344,23 @@ const arxiv = async (checks) => {
 };
 
 $(() => {
-    const url = window.location.href;
-    info("Executing Paper Memory content script");
     if (
         Object.values(global.knownPaperPages)
             .reduce((a, b) => a.concat(b), [])
             .some((d) => url.includes(d))
     ) {
-        info("Running contentScriptMain for", url);
+        const url = window.location.href;
+        info("Running PaperMemory's contentScriptMain for:", url);
         contentScriptMain(url);
     }
 
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         // listen for messages sent from background.js
         if (request.message === "tabUrlUpdate") {
-            info("Running content_script for url update");
-            console.log(request.url); // new url is now in content scripts!
+            info(
+                "Running PaperMemory's contentScriptMain for url update:",
+                request.url
+            );
             contentScriptMain(request.url);
         }
     });
