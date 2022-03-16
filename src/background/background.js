a0838cce169d03e0629256fd8480e04b8a0dd06d
vict0rsch <vsch@pm.me>
2022-03-16 10:35:33 +0100

    send message on known page only

--- src/background/background.js
+++ src/background/background.js
@@ -109,9 +109,12 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     // read changeInfo data and do something with it
     // like send the new url to contentscripts.js
     if (changeInfo.url) {
-        chrome.tabs.sendMessage(tabId, {
-            message: "tabUrlUpdate",
-            url: changeInfo.url,
-        });
+        const is = isPaper(changeInfo.url);
+        if (Object.values(is).some((i) => i)) {
+            chrome.tabs.sendMessage(tabId, {
+                message: "tabUrlUpdate",
+                url: changeInfo.url,
+            });
+        }
     }
 });
