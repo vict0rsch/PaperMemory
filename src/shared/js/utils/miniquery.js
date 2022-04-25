5356bf9b4aff7599d84a816cec68f624a102139a
vict0rsch <vsch@pm.me>
2022-04-25 15:48:02 +0200

    add `createElementFromHTML`

--- src/shared/js/utils/miniquery.js
+++ src/shared/js/utils/miniquery.js
@@ -261,3 +261,11 @@ if (typeof module !== "undefined" && module.exports != null) {
         slideToggle,
     };
 }
+
+const createElementFromHTML = (htmlString) => {
+    var div = document.createElement("div");
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstChild;
+};
