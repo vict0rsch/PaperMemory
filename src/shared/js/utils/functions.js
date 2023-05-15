05a29ce65457450f9bc6fe1692b83fe265fc17cc
vict0rsch <vsch@pm.me>
2023-05-15 17:59:49 -0400

    smiley face from tabler icons

--- src/shared/js/utils/functions.js
+++ src/shared/js/utils/functions.js
@@ -509,6 +509,15 @@ const tablerSvg = (pathName, id, classNames) => {
                 <line x1="5" y1="12" x2="11" y2="12" />
             </svg>`;
 
+        case "huggingface":
+            return `<svg viewBox="0 0 24 24" ${id} ${classNames}>
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" />
+                    <path d="M10 10c-.5 -1 -2.5 -1 -3 0" />
+                    <path d="M17 10c-.5 -1 -2.5 -1 -3 0" />
+                    <path d="M14.5 15a3.5 3.5 0 0 1 -5 0" />
+            </svg>`;
+
         default:
             return "";
     }
