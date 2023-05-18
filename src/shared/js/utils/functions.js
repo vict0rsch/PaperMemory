870085f23089581425f996adc963c7417cf2acd9
vict0rsch <vsch@pm.me>
2023-05-18 10:26:24 -0400

    use remote svg + grayscale css filter

--- src/shared/js/utils/functions.js
+++ src/shared/js/utils/functions.js
@@ -510,13 +510,12 @@ const tablerSvg = (pathName, id, classNames) => {
             </svg>`;
 
         case "huggingface":
-            return `<svg viewBox="0 0 24 24" ${id} ${classNames}>
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" />
-                    <path d="M10 10c-.5 -1 -2.5 -1 -3 0" />
-                    <path d="M17 10c-.5 -1 -2.5 -1 -3 0" />
-                    <path d="M14.5 15a3.5 3.5 0 0 1 -5 0" />
-            </svg>`;
+            return `<img
+                src="https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg"
+                style="filter: grayscale(1)"
+                ${id}
+                ${classNames}
+            >`;
 
         default:
             return "";
