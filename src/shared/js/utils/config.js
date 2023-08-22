212a50b8bd11a5816b79f2d4d749897dac3519c9
vict0rsch <vsch@pm.me>
2023-08-22 18:46:57 -0400

    🐛 (source): Improve NeurIPS parsing
    Include `/paper_files/` sub-path

--- src/shared/js/utils/config.js
+++ src/shared/js/utils/config.js
@@ -172,7 +172,7 @@ global.knownPaperPages = {
     iop: ["iopscience.iop.org/article/"],
     jmlr: [(url) => url.includes("jmlr.org/papers/v") && !url.endsWith("/")],
     nature: ["nature.com/articles/"],
-    neurips: ["neurips.cc/paper/", "nips.cc/paper/"],
+    neurips: ["neurips.cc/paper/", "neurips.cc/paper_files/paper/", "nips.cc/paper/"],
     openreview: [
         "openreview.net/forum",
         "openreview.net/pdf",
