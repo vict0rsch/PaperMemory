ffcef6d03c9b194022ebba533f4e22c7fdd74e80
vict0rsch <vsch@pm.me>
2024-01-11 18:55:35 -0500

    ⚡️ (tests): Improve test utils with `root`

--- test/utilsForTests.js
+++ test/utilsForTests.js
@@ -2,6 +2,8 @@ const glob = require("glob");
 const fs = require("fs");
 const ora = require("ora");
 
+exports.root = fs.existsSync("./manifest.json") ? "." : "..";
+
 exports.sleep = async (duration, textToDisplay) => {
     const text = textToDisplay
         ? `${textToDisplay} (${duration / 1e3}s)`
@@ -12,7 +14,11 @@ exports.sleep = async (duration, textToDisplay) => {
 };
 
 exports.loadPaperMemoryUtils = () => {
-    const utilsFiles = glob.sync("../src/shared/js/utils/*.js");
+    const utilsFiles = glob
+        .sync(`${this.root}/src/shared/js/utils/*.js`)
+        .filter((file) => !file.endsWith("gist.js") && !file.endsWith("sync.js"))
+        .map((file) => `../${file.replace(".js", "")}`);
+    console.log("utilsFiles: ", utilsFiles);
     const utilsModules = utilsFiles.map((file) => require(file));
 
     for (const module of utilsModules) {
@@ -27,3 +33,7 @@ exports.range = (n) => [...Array(n).keys()];
 exports.readJSON = (fname) => JSON.parse(fs.readFileSync(fname));
 
 exports.asyncMap = (arr, func) => Promise.all(arr.map(func));
+
+exports.readURLs = () => this.readJSON(`${this.root}/test/data/urls.json`);
+
+exports.readDuplicates = () => this.readJSON(`${this.root}/test/data/duplicates.json`);
