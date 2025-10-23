e987c8ca8628e7d6927ce48a2b1b3a60cd8c147f
vict0rsch <victor.schmidt@entalpic.ai>
2025-10-23 16:49:27 +0200

    🐛 fix: Add env variable to control tmpBrowser.js

--- test/tmpBrowser.js
+++ test/tmpBrowser.js
@@ -1,3 +1,13 @@
 import { makeBrowser } from "./browser.js";
 
-await makeBrowser();
+const openBrowser = process.env.openBrowser ?? false;
+
+if (openBrowser) {
+    const browser = await makeBrowser();
+    const p = (await browser.pages())[0];
+    p.on("console", (msg) => {
+        console.log("msg.text() :", msg.text());
+    });
+} else {
+    console.log("Set `openBrowser` to `true` to open a browser");
+}
